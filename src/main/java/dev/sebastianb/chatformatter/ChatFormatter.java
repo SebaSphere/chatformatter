fa9df6aef051888bb9554ceb267c4d47b2bf7145
SebaSphere <SebaSphereEmails@gmail.com>
2021-07-25 18:58:34 -0400

    Initial commit

---
+++ src/main/java/dev/sebastianb/chatformatter/ChatFormatter.java
@@ -0,0 +1,12 @@
+package dev.sebastianb.chatformatter;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+
+public class ChatFormatter implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
